6edeb48e7b24f17b4fd7bb6c348a9ce315bf027c
iamapusaha <apusaha392@gmail.com>
2023-11-05 16:42:42 +0600

    get job data by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -28,6 +28,7 @@ async function run() {
         // Send a ping to confirm a successful connection
         const jobsCollection = client.db('jobsDB').collection('jobs')
 
+        // job related api 
         app.post('/jobs', async (req, res) => {
             const newJob = req.body;
             const result = await jobsCollection.insertOne(newJob);
@@ -37,6 +38,12 @@ async function run() {
             const category = req.params.category;
             const query = { category: category }
             const result = await jobsCollection.find(query).toArray();
+            res.send(result)
+        })
+        app.get('/jobs/v1/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await jobsCollection.findOne(query)
             res.send(result)
         })
 
